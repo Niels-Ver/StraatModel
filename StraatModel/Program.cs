1f99a13d6cbc97c8692edaadfb991c371b876999
Niels Verbraeken <niels_verbraeken@hotmail.com>
2020-02-17 13:46:39 +0100

    Add project files.

---
+++ StraatModel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace StraatModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //string line;
+            //using (StreamReader r = new StreamReader(path))
+            //{
+            //    while((line = r.ReadLine()) != null)
+            //    {
+
+            //    }
+            //}
+        }
+    }
+}
